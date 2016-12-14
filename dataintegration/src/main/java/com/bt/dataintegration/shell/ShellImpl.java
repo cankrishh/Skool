412b0fe82ce76defe5cee5dbfe0cad68226d805e
abhimegh <abhinav.meghmala@bt.com>
2016-12-14 11:29:46 +0530

    Update ShellImpl.java

--- dataintegration/src/main/java/com/bt/dataintegration/shell/ShellImpl.java
+++ dataintegration/src/main/java/com/bt/dataintegration/shell/ShellImpl.java
@@ -202,39 +202,27 @@ public class ShellImpl implements IShell {
 	}
 
 	public String housekeepShell() {
+		
 		String shellCmd = "RET_DAYS_RAW_DATA=$1\n"
 				+ "PATH_RAW_DATA=$2\n"
 				+ "#RET_DAYS_PROCESSED_DATA=$3\n"
 				+ "#PATH_PROCESSED_DATA=$4\n"
-				+ "if [ -z \"$RET_DAYS_RAW_DATA\"] && [\"$RET_DAYS_RAW_DATA\" == \"\"] && [ -z \"$PATH_RAW_DATA\"] && [\"$PATH_RAW_DATA\" == \"\"]; then\n"
+				+ "if [ -z \"$RET_DAYS_RAW_DATA\" ] && [ \"$RET_DAYS_RAW_DATA\" == \"\" ] && [ -z \"$PATH_RAW_DATA\" ] && [ \"$PATH_RAW_DATA\" == \"\" ]; then\n"
 				+ "echo \"empty\"\n"
 				+ "else\n"
-				+ "date_threshold=`date -d ' $RET_DAYS_RAW_DATA days' +'%Y/%m/%d'`\n"
-				+ "for i in `hadoop fs -ls -R $PATH_RAW_DATA | awk '{print $1\"|\" $8}'`\n"
+				+ "date_threshold=$(date -d \"-$RET_DAYS_RAW_DATA days\" +'%Y/%m/%d')\n"
+				+ "for i in `hadoop fs -ls -R $PATH_RAW_DATA | awk '{print $1\"|\"$6\"|\"$8}'`\n"
 				+ "do \n"
-				+ "DIRECTORY_NAME=${i:$((${#2}+11))}\n"
+				+ "IFS=\"|\"; declare -a Array=($i)\n"
+				+ "DIRECTORY_DATE=${Array[1]}\n"
+				+ "DIRECTORY_NAME=${Array[2]}\n"
 				+ "#echo $DIRECTORY_NAME\n"
-				+ "if [[ ${i:0:1} == 'd' && ${#DIRECTORY_NAME} -eq 10 ]]; then \n"
+				+ "if [[ ${i:0:1} == 'd' && ${#DIRECTORY_DATE} -eq 10 ]]; then \n"
 				+ "#echo $DIRECTORY_NAME; \n"
-				+ "if [[ $DIRECTORY_NAME < $date_threshold ]]; then \n"
-				+ "hadoop fs -rm -r $PATH_RAW_DATA$DIRECTORY_NAME\n" + "fi\n"
+				+ "if [[ $DIRECTORY_DATE < $date_threshold ]]; then \n"
+				+ "hadoop fs -rm -r $DIRECTORY_NAME\n" + "fi\n"
 				+ "fi\n" + "done\n" + "fi\n";
-
-		/*
-		 * +
-		 * "if [ -z \"$RET_DAYS_PROCESSED_DATA\"] && [\"$RET_DAYS_PROCESSED_DATA\" == \"\"] && [ -z \"$PATH_PROCESSED_DATA\"] && [\"$PATH_PROCESSED_DATA\" == \"\"]; then\n"
-		 * +"echo \"empty\"\n" +"else\n" +
-		 * "date_threshold=`date -d ' $RET_DAYS_PROCESSED_DATA days' +'%Y/%m/%d'`\n"
-		 * +
-		 * "for i in `hadoop fs -ls -R $PATH_PROCESSED_DATA | awk '{print $1\"|\" $8}'`\n"
-		 * +"do \n" +"DIRECTORY_NAME=${i:$((${#2}+11))}\n"
-		 * +"#echo $DIRECTORY_NAME\n"
-		 * +"if [[ ${i:0:1} == 'd' && ${#DIRECTORY_NAME} -eq 10 ]]; then \n"
-		 * +"#echo $DIRECTORY_NAME; \n"
-		 * +"if [[ $DIRECTORY_NAME < $date_threshold ]]; then \n"
-		 * +"hadoop fs -rm -r $PATH_PROCESSED_DATA$DIRECTORY_NAME\n" +"fi\n"
-		 * +"fi\n" +"done\n" +"fi\n";
-		 */
+		
 		return shellCmd;
 	}
 
